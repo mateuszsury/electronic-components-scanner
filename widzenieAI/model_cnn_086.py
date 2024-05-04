1b47a4c927ef43ca171867d0bbf887b5cc766b8e
mateuszsury <66475105+mateuszsury@users.noreply.github.com>
2024-05-04 15:34:32 +0200

    Add files via upload

---
+++ widzenieAI/model_cnn_086.py
@@ -0,0 +1,123 @@
+import rembg
+import numpy as np
+from PIL import Image, ImageFilter, ImageEnhance
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+import torchvision.transforms as transforms
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class TextRecognitionCNN(nn.Module):
+    def __init__(self, num_chars):
+        super(TextRecognitionCNN, self).__init__()
+        self.features = models.resnet50(pretrained=True) 
+        self.features.fc = nn.Linear(2048, 512) 
+        self.fc = nn.Linear(512, num_chars) 
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.fc(x)
+        return x
+
+def preprocess_image(image_path):
+    input_image = Image.open(image_path)
+
+    # filtr wyostrzający
+    input_image_sharpened = input_image.filter(ImageFilter.UnsharpMask(radius=SHARPEN_RADIUS, percent=150, threshold=3))
+
+    # kontrast
+    contrast_enhancer = ImageEnhance.Brightness(input_image_sharpened)
+    input_image_enhanced = contrast_enhancer.enhance(2)
+
+    # konwersja do numpy array
+    input_array = np.array(input_image_enhanced)
+
+    # usuwanie tła
+    output_array = rembg.remove(input_array)
+
+    # konwersja do RGB przed zapisaniem
+    output_image_rgb = Image.fromarray(output_array).convert('RGB')
+
+    # normalizacja 
+    preprocess = transforms.Compose([
+        transforms.Resize((TARGET_HEIGHT, TARGET_WIDTH)),  # Adjust size
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    return preprocess(output_image_rgb)
+
+
+SHARPEN_RADIUS = 1
+TARGET_WIDTH = 200
+TARGET_HEIGHT = 200
+NUM_CHARS = 128  
+
+
+model = TextRecognitionCNN(NUM_CHARS)
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# odczyy danych
+data = pd.read_csv('training_data.csv', sep=',', names=['Image_Path', 'Text', 'Is_Correct'])
+data = data.dropna(subset=['Text'])
+
+# przetwarzanie obrazów
+X = []
+y = []
+for index, row in data.iterrows():  
+    image_path = row['Image_Path']
+    text = row['Text']
+    is_correct = row['Is_Correct']
+    image = preprocess_image(image_path)  
+    X.append(image)  
+    y.append(1 if is_correct == 'y' else 0)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# kowersja danych na tensory pytorch
+X_train = torch.stack(X_train).to(device)
+X_test = torch.stack(X_test).to(device)
+
+def label_to_one_hot(label, num_classes):
+    one_hot = torch.zeros(num_classes)
+    one_hot[label] = 1
+    return one_hot
+
+y_train = torch.stack([label_to_one_hot(label, NUM_CHARS) for label in y_train]).to(device)
+y_test = torch.stack([label_to_one_hot(label, NUM_CHARS) for label in y_test]).to(device)
+
+# określenie optymalizatora i funkcji straty
+criterion = nn.CrossEntropyLoss()
+# criterion = nn.BCEWithLogitsLoss()
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+  
+# pętla trenowania i dostosowywanie epok trenowania
+num_epochs = 30
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    outputs = model(X_train)
+    loss = criterion(outputs.view(-1, NUM_CHARS), y_train)
+    loss.backward()
+    optimizer.step()
+
+    # Print training loss
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+
+# ewaluacja do określenia dokładności
+model.eval()
+with torch.no_grad():
+    outputs = model(X_test)
+    predicted = torch.argmax(outputs, dim=1)
+    accuracy = torch.mean((predicted == torch.argmax(y_test, dim=1)).float())
+    print(f"Accuracy: {accuracy}")
+
+# zapis modelu
+torch.save(model.state_dict(), 'text_recognition_model.pth')
+print("Model saved.")
---
+++ widzenieAI/wektoryzacja_mutlinomial.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import os
+import rembg
+import numpy as np
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import easyocr
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import csv
+import os
+folder_path = r'C:\Users\mklim\Desktop\Informatyczne systemy automatyki\electronic-components-scanner-main\ai_jpg'
+
+def find_bounding_box(image_array):
+    rows = np.any(image_array != [0, 0, 0, 0], axis=(1, 2))
+    cols = np.any(image_array != [0, 0, 0, 0], axis=(0, 2))
+
+    min_row, max_row = np.where(rows)[0][[0, -1]]
+    min_col, max_col = np.where(cols)[0][[0, -1]]
+
+    return min_row, min_col, max_row, max_col
+
+def preprocess_image(image_path):
+    input_image = Image.open(image_path)
+
+    # filtr wyostrzający z dostosowanym promieniem
+    input_image_sharpened = input_image.filter(ImageFilter.UnsharpMask(radius=SHARPEN_RADIUS, percent=150, threshold=3))
+
+    # kontrast
+    contrast_enhancer = ImageEnhance.Brightness(input_image_sharpened)
+    input_image_enhanced = contrast_enhancer.enhance(2)
+
+    # konwersja do numpy array
+    input_array = np.array(input_image_enhanced)
+
+    # usuwanie tła
+    output_array = rembg.remove(input_array)
+
+    # znajdź granice tekstu
+    min_row, min_col, max_row, max_col = find_bounding_box(output_array)
+
+    # wytnij obszar z tekstem
+    output_array_cropped = output_array[min_row:max_row, min_col:max_col]
+
+    # przeskaluj obraz
+    output_image_resized = Image.fromarray(output_array_cropped).resize((TARGET_WIDTH, TARGET_HEIGHT), resample=Image.BILINEAR)
+
+    # konwersja do RGB przed zapisaniem
+    output_image_resized_rgb = output_image_resized.convert('RGB')
+
+    return np.array(output_image_resized_rgb)  # Zwróć obraz jako tablicę numpy
+# promień wyostrzania
+SHARPEN_RADIUS = 1
+TARGET_WIDTH = 200
+TARGET_HEIGHT = 200
+
+# inicjalizacja easyocr
+reader = easyocr.Reader(['en'])
+
+training_data_path = 'training_data.csv'
+
+# wczytanie danych
+data = pd.read_csv('training_data.csv', sep=',', names=['Image_Path', 'Text', 'Is_Correct'])
+
+# przetwarzanie obrazów
+X = []
+y = []
+for index, row in data.iterrows():  
+    image_path = row['Image_Path']
+    text = row['Text']
+    is_correct = row['Is_Correct']
+    image = preprocess_image(image_path)  
+    X.append(np.array(image))  
+    y.append(1 if is_correct == 'y' else 0)
+
+X = np.array(X)
+
+
+X_train_img, X_test_img, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+data = data.dropna(subset=['Text'])
+
+# wektoryzacja tekstu
+tfidf_vectorizer = TfidfVectorizer()
+X_train_text_tfidf = tfidf_vectorizer.fit_transform(data['Text'])
+
+X_test_text_tfidf = tfidf_vectorizer.transform(data['Text'])
+
+classifier = MultinomialNB()
+classifier.fit(X_train_text_tfidf, y_train)
+
+y_pred = classifier.predict(X_test_text_tfidf)
+
+accuracy = accuracy_score(y_test, y_pred[:len(y_test)])
+print(f"Accuracy: {accuracy}")
